9feecf9a22946f317bfb53475b7c9feb0fe58ecb
h-cue <hh222nx@student.lnu.se>
2020-08-17 13:21:15 +0200

    testing of grid sizes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useCallback, useRef } from "react";
 import produce from "immer";
 
-const numRows = 50;
-const numCols = 50;
+const numRows = 60;
+const numCols = 60;
 
 const operations = [
   [0, 1],
